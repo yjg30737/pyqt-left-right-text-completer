c2925c118afce096ea191855d2fb993b4921cb14
Jung Gyu Yoon <yjg30737@gmail.com>
2022-01-05 09:19:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Left text, right text Completer. Left text is major target of search.',
+    description='PyQt Left text, right text completer. Left text is major target of search.',
     url='https://github.com/yjg30737/pyqt-left-right-text-completer.git',
     install_requires=[
         'PyQt5>=5.8'
