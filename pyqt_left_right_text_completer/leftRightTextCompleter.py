d8c83d1185e465794fb1e04201987bb3ffe098ca
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 17:48:01 +0900

    Update setup.py

--- pyqt_left_right_text_completer/leftRightTextCompleter.py
+++ pyqt_left_right_text_completer/leftRightTextCompleter.py
@@ -52,4 +52,3 @@ class LeftRightTextCompleter(QCompleter):
 
 
 
-
--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-left-right-text-completer',
@@ -7,8 +15,10 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt Left text, right text completer. Left text is major target of search.',
+    description='PyQt left text, right text completer. Left text is major target of search.',
     url='https://github.com/yjg30737/pyqt-left-right-text-completer.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
